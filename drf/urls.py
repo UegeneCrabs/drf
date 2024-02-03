5a940b06a39c4d9a8929e46eefa846dd1780054d
Zhenya <yevgeniy.meyntser@mail.ru>
2024-02-03 23:51:16 +0300

    14 lesson

--- drf/urls.py
+++ drf/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from drfsite.views import TaskViewSet
 
@@ -28,8 +29,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('rest_framework.urls')),
     path('api/v1/', include(router.urls)),
-    path(r'api/v1/auth/', include('djoser.urls')),
-    re_path(r'auth/', include('djoser.urls.authtoken')),
+    # path(r'api/v1/auth/', include('djoser.urls')),
+    # re_path(r'auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 #5d20d0f52ac5b9f8b5fd7242447c54dd94bc58b3 - test
 #2871b6f6ccf8d25aaf72de5ba2571425c7ec9b95 - admin
