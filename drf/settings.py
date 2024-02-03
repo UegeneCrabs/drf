e08ab87f270710ac686449a9f9e1c1b88eac974a
Zhenya <yevgeniy.meyntser@mail.ru>
2024-02-03 23:57:59 +0300

    15 lesson

--- drf/settings.py
+++ drf/settings.py
@@ -127,11 +127,13 @@ REST_FRAMEWORK = {
     ),
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        #'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 SIMPLE_JWT = {
--- drfsite/views.py
+++ drfsite/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
@@ -9,17 +10,21 @@ from .permissions import CustomPermission
 from .serializers import TaskSerializer, StoreSerializer
 
 
+class MyPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = (CustomPermission,)
-    #authentication_classes = (TokenAuthentication,)
+    # authentication_classes = (TokenAuthentication,)
+    pagination_class = MyPagination
 
-    # ------------------------------8:9 lesson-------------------------#
     @action(methods=['get'], detail=False)
     def store(self, request):
         stores = Store.objects.all()
         serializer = StoreSerializer(stores, many=True)
         return Response({'stores': serializer.data})
-
-# ------------------------------other lesson-------------------------#
